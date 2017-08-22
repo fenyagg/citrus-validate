f3ff50fab64138c51fe9b3a16a7fe69d092e35c3
fenyagg <fenyagg@gmail.com>
2017-08-22 09:57:28 +0300

    v1.4.4 добавил дестрой через jquery плагин
    $('form').citrusValidator('destroy');

--- src/js/citrusValidatorPlugin.js
+++ src/js/citrusValidatorPlugin.js
@@ -4,7 +4,11 @@ $.fn.citrusValidator = function(params) {
 	var $el = this;
 	if (isset(params) && params === 'get') {
 		return proto._getValidator($el);
-	} else {
+	} else if(isset(params) && params === 'destroy'){
+		$(proto._getValidator($el)).each(function (index, validator) {
+			validator.destroy();
+		});
+	}else {
 		var params = params || {};
 		return $el.each(function(index, form ) {
 			new citrusValidator($(form), params);
